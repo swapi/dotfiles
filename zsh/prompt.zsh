72483eebed4b7859785aafde570caa7f10fc9799
swapi <swapnil@hardcodr.com>
2014-12-05 21:55:26 +0530

    iTerm themes and zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -67,11 +67,15 @@ rb_prompt() {
   fi
 }
 
+preamble() {
+    echo "%{$fg_bold[yellow]%}%n@%m%{$reset_color%} "
+}
+
 directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(preamble)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
